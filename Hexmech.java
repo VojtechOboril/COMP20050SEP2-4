af61dcfc73fde6b729cd96c3fd4b6a3aa84c3e21
MattyPc <mateusz.jakobik@ucdconnect.ie>
2024-03-08 09:23:43 +0000

    Created a method that currently prints location of atoms when pressing the new "revealAtoms" button. Still need to: Make it click/unclick the atoms

--- Hexmech.java
+++ Hexmech.java
@@ -34,6 +34,7 @@ http://www.tonypa.pri.ee/tbw/tut25.html
     private static int r=0;	// radius of inscribed circle (centre to middle of each side). r= h/2
     private static int h=0;	// height. Distance between centres of two adjacent hexes. Distance between two opposite sides in a hex.
 
+    private static int w=0;
     public static void setXYasVertex(boolean b) {
         XYVertex=b;
     }
@@ -47,20 +48,21 @@ http://www.tonypa.pri.ee/tbw/tut25.html
     //calculating middle location of circle for circles of influence
     public static Point hexToPixel(int x, int y) {
         int mx = (int) (x * (s + t) - (BORDERS)+3);
-        int my = (int) (y * h + (x % 2) * r - BORDERS);
+        int my = (int) (y * h + (x % 2) * r - BORDERS+3);
         return new Point(mx, my);
     }
-    public static void setSide(int side) {
-        s=side;
-        t =  (int) (s / 2);			//t = s sin(30) = (int) CalculateH(s);
-        r =  (int) (s * 0.8660254037844);	//r = s cos(30) = (int) CalculateR(s);
-        h=2*r;
-    }
+    /* public static void setSide(int side) {
+         s=side;
+         t =  (int) (s / 2);			//t = s sin(30) = (int) CalculateH(s);
+         r =  (int) (s * 0.8660254037844);	//r = s cos(30) = (int) CalculateR(s);
+         h=2*r;
+     }*/
     public static void setHeight(int height) {
         h = height;			// h = basic dimension: height (distance between two adj centresr aka size)
         r = h/2;			// r = radius of inscribed circle
         s = (int) (h / 1.73205);	// s = (h/2)/cos(30)= (h/2) / (sqrt(3)/2) = h / sqrt(3)
         t = (int) (r / 1.73205);	// t = (h/2) tan30 = (h/2) 1/sqrt(3) = h / (2 sqrt(3)) = r / sqrt(3)
+        w = (int) (h*Math.sqrt(3)/2);
     }
 
     /*********************************************************
@@ -76,8 +78,8 @@ http://www.tonypa.pri.ee/tbw/tut25.html
      *********************************************************/
     public static Polygon hex (int x0, int y0) {
 
-        int y = y0 + BORDERS;
-        int x = x0 + BORDERS; // + (XYVertex ? t : 0); //Fix added for XYVertex = true.
+        int topY = y0 + BORDERS;
+        int leftX = x0+BORDERS; // + (XYVertex ? t : 0); //Fix added for XYVertex = true.
         // NO! Done below in cx= section
         if (s == 0  || h == 0) {
             System.out.println("ERROR: size of hex has not been set");
@@ -87,12 +89,12 @@ http://www.tonypa.pri.ee/tbw/tut25.html
         int[] cx,cy;
 
 //I think that this XYvertex stuff is taken care of in the int x line above. Why is it here twice?
-        if (XYVertex)
-            cx = new int[] {x,x+s,x+s+t,x+s,x,x-t};  //this is for the top left vertex being at x,y. Which means that some of the hex is cutoff.
-        else
-            cx = new int[] {x+t,x+s+t,x+s+t+t,x+s+t,x+t,x};	//this is for the whole hexagon to be below and to the right of this point
+        // if (XYVertex)
+        // cx = new int[] {x,x+s,x+s+t,x+s,x,x-t};  //this is for the top left vertex being at x,y. Which means that some of the hex is cutoff.
+        // else
+        cx = new int[] {leftX + w / 2,leftX + w,leftX + w,leftX + w / 2,leftX,leftX};	//this is for the whole hexagon to be below and to the right of this point
 
-        cy = new int[] {y,y,y+r,y+r+r,y+r+r,y+r};
+        cy = new int[] {topY,topY + h / 4,topY + h / 4 * 3,topY + h,topY + h / 4 * 3,topY + h / 4};
         return new Polygon(cx,cy,6);
 
 		/*
