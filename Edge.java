888c94d784836b4de04763a58cdeb1eea1691f7b
Vojtech Oboril <vojtech.oboril@gmail.com>
2024-03-27 11:40:29 +0000

    fixed 2 edge cases

--- Edge.java
+++ Edge.java
@@ -12,15 +12,20 @@ public class Edge extends Tile {
                 Ray r = new Ray(this, i);
                 System.out.println("Ray started at " + this + " with the direction of " + r.getDirection());
 
-                // check if there is an atom right next to it, resulting in a reflection
+                // check if there is an atom right next to it, resulting in a reflection or absorbtion
                 boolean reflected = false;
+                boolean absorbed = false;
                 for(int j = r.getDirection() - 1; j <= r.getDirection()+1; j++) {
                     // if one of the tiles in the direction of the beam has an atom
-                    if(this.getAdjacent(j%6) instanceof Hexagon && ((Hexagon) this.getAdjacent(j%6)).getActive()) {
+                    if(this.getAdjacent((j + 6)%6) instanceof Hexagon && ((Hexagon) this.getAdjacent((j + 6)%6)).getActive()) {
+                        if(j == r.getDirection()) absorbed = true;
                         reflected = true;
                     }
                 }
-                if(reflected) {
+                if(absorbed) {
+                    r.setEnd(this.getAdjacent(r.getDirection()));
+                    r.setResult(Result.ABSORBED);
+                } else if(reflected) {
                     this.receiveRay(r);
                 } else {
                     this.getAdjacent(r.getDirection()).receiveRay(r);
