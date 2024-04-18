dce8f2adcf00d98dfc44137d770a17dd41713034
Mateusz Jakobik <mateusz.jakobik@ucdconnect.ie>
2024-04-18 19:19:39 +0100

    Errors: bottom right edges inverted?? A raymarker gets placed at the end of the ray in the same half of hexagon as ray start Otherwise the raymarkers look good

--- Edge.java
+++ Edge.java
@@ -1,7 +1,9 @@
 import java.awt.*;
+import java.util.Objects;
 
 public class Edge extends Tile {
-    private int rayCounter;
+    private int rayCounterTop;
+    private int rayCounterBottom;
     private boolean clickedTopHalf;
     public static int globalRayCounter = 0;
     private Point topSquarePosition;
@@ -10,33 +12,43 @@ public class Edge extends Tile {
     private Point bottomSquarePosition;
     private Point bottomSquarePositionStart;
     private Point bottomSquarePositionEnd;
-
-    public Edge(int x, int y, int z) {
-        super(x, y, z);
-        this.rayCounter = 0;
-        this.topSquarePosition = null;
-        this.bottomSquarePosition = null;
-        this.topSquarePositionStart = null;
-        this.bottomSquarePositionStart = null;
-        this.topSquarePositionEnd = null;
-        this.bottomSquarePositionEnd = null;
-        this.endTile = null;
-    }
-
-
     public static int locationOnHex;
     public static boolean rightHalf = false;
 
     public static int locationOnFullHex;
     public static boolean betweenTopAndBot = false;
     public static boolean bottomOfHex = false;
+    public String topColourBox;
+    public String botColourBox;
 
-    public static Tile startTile;
-    public static Tile endTile = null;
+    public  static Edge startTile;
+    public  static Edge endTile = null;
+
+    public Edge(int x, int y, int z) {
+        super(x, y, z);
+        this.rayCounterTop = 0;
+        this.rayCounterBottom = 0;
+        this.topSquarePosition = null;
+        this.bottomSquarePosition = null;
+        this.topSquarePositionStart = null;
+        this.bottomSquarePositionStart = null;
+        this.topSquarePositionEnd = null;
+        this.bottomSquarePositionEnd = null;
+        this.topColourBox = null;
+        this.botColourBox = null;
+        this.endTile = null;
+        this.startTile = null;
+    }
+
 
 
     @Override
     public void clicked() {
+        this.endTile = null;
+        this.startTile = null;
+
+
+
         System.out.println("this is an edge");
         if (locationOnHex == 0) {
             this.clickedTopHalf = true;
@@ -88,7 +100,8 @@ public class Edge extends Tile {
                 }
 
                 System.out.println("Ray started at " + this + " with the direction of " + r.getDirection());
-                startTile = this;
+                this.startTile = this;
+                this.endTile = this;
                 // check if there is an atom right next to it, resulting in a reflection or absorbtion
                 boolean reflected = false;
                 boolean absorbed = false;
@@ -96,51 +109,74 @@ public class Edge extends Tile {
                     // if one of the tiles in the direction of the beam has an atom
                     if (this.getAdjacent((j + 6) % 6) instanceof Hexagon && ((Hexagon) this.getAdjacent((j + 6) % 6)).getActive()) {
                         if (j == r.getDirection()) absorbed = true;
-                        reflected = true;
+                        this.endTile = null;
+                            reflected = true;
                     }
                 }
                 if (absorbed) {
                     r.setEnd(this.getAdjacent(r.getDirection()));
                     r.setResult(Result.ABSORBED);
-                    this.endTile = null;
                 } else if (reflected) {
                     this.receiveRay(r);
                 } else {
                     this.getAdjacent(r.getDirection()).receiveRay(r);
                 }
 
+                if(r.getResult()==Result.ABSORBED) {
+                    this.endTile = null;
+                    System.out.println("ABSORBED AND ENDTILE SHOULD BE NULL");
+                }
                 //startTile = r.getStart();
                 //this prevents function from running twice when we have more than one adjacent hexagon
                 break;
             }
         }
 
+
         int hexMidStartY = Hexmech.hexToPixel(this.startTile.p.x, this.startTile.p.y).y;
         int hexMidStartX = Hexmech.hexToPixel(this.startTile.p.x, this.startTile.p.y).x;
+
+
+        if(this.endTile == null){
+            this.endTile = this.startTile;
+        }
+
 
         int hexMidEndY = Hexmech.hexToPixel(this.endTile.p.x, this.endTile.p.y).y;
         int hexMidEndX = Hexmech.hexToPixel(this.endTile.p.x, this.endTile.p.y).x;
 
-        this.rayCounter = ++globalRayCounter;
         if (this.clickedTopHalf) {
-            if ((this.topSquarePosition == null)&&(this.topSquarePositionStart == null)&&(this.topSquarePositionEnd == null)) {
-                this.topSquarePositionStart = new Point(hexMidStartX + 55, hexMidStartY  + 40);
-                if(endTile!=null) {
-                    this.topSquarePositionEnd = new Point(hexMidEndX + 55, hexMidEndY + 40);
+            if(rayCounterTop == 0) {
+                this.rayCounterTop = ++globalRayCounter;
+                if ((this.topSquarePosition == null) && (this.topSquarePositionStart == null) && (this.topSquarePositionEnd == null)) {
+                    if(startTile == endTile){
+                        topColourBox = "Red";
+                    }else{
+                        topColourBox = "Blue";
+                    }
+                    this.topSquarePositionStart = new Point(hexMidStartX + 55, hexMidStartY + 40);
+                    if (this.endTile != null) {
+                        this.topSquarePositionEnd = new Point(hexMidEndX + 55, hexMidEndY + 40);
+                    }
+                    this.topSquarePosition = new Point(1, 1);
                 }
-                this.topSquarePosition = new Point(1,1);
-            } else {
-                globalRayCounter--;
             }
-        } else {
-            if (this.bottomSquarePosition == null&&(this.bottomSquarePositionStart == null)&&(this.bottomSquarePositionEnd == null)) {
-                this.bottomSquarePositionStart = new Point(hexMidStartX + 55, hexMidStartY + 70);
-                if(this.endTile!=null) {
-                    this.bottomSquarePositionEnd = new Point(hexMidEndX + 55, hexMidEndY + 70);
+        }else {
+            if (rayCounterBottom == 0) {
+                this.rayCounterBottom = ++globalRayCounter;
+                if (this.bottomSquarePosition == null && (this.bottomSquarePositionStart == null) && (this.bottomSquarePositionEnd == null)) {
+                    if(startTile == endTile){
+                        botColourBox = "Red";
+                    }
+                    else{
+                        botColourBox = "Blue";
+                    }
+                    this.bottomSquarePositionStart = new Point(hexMidStartX + 55, hexMidStartY + 70);
+                    if (this.endTile != null) {
+                        this.bottomSquarePositionEnd = new Point(hexMidEndX + 55, hexMidEndY + 70);
+                    }
+                    this.bottomSquarePosition = new Point(1, 1);
                 }
-                this.bottomSquarePosition = new Point(1,1);
-            } else {
-                globalRayCounter--;
             }
         }
     }
@@ -156,16 +192,16 @@ public class Edge extends Tile {
         }
     }
 
+    @Override
     public void drawBottom(Graphics2D g2, boolean showCircles) {
         // showCircles unused here
         // TODO change this to draw it in 2 halves
         Hexmech.drawHex(this.p.x, this.p.y, g2);
         Hexmech.fillHex(this.p.x, this.p.y, this.value, g2);
     }
-
+    @Override
     void drawTop(Graphics2D g2, boolean showCircles) {
-        //draw any initial lines that went through if showCircles
-        if (startTile != null && endTile != null) {
+        if (this.startTile != null && this.endTile != null) {
             drawRayMarker(g2);
         }
     }
@@ -174,38 +210,52 @@ public class Edge extends Tile {
 
     @Override
     void drawRayMarker(Graphics2D g2) {
-        System.out.println(endTile);
+        //System.out.println(this.endTile);
 //        int hexMidY = Hexmech.hexToPixel(this.p.x, this.p.y).y;
 //        int hexMidX = Hexmech.hexToPixel(this.p.x, this.p.y).x;
         g2.setColor(Color.blue);
 
         // Draw the top square if it exists
         if (this.topSquarePosition != null) {
-            drawSquare(g2, this.topSquarePositionStart, this.rayCounter);
+            drawSquare(g2, this.topSquarePositionStart, this.rayCounterTop);
+            if (Objects.equals(this.topColourBox, "Blue")) {
+                g2.setColor(Color.blue);
+            } else {
+                g2.setColor(Color.red);
+            }
             if(this.endTile!=null) {
-                drawSquare(g2, this.topSquarePositionEnd, this.rayCounter);
+                drawSquare(g2, this.topSquarePositionEnd, this.rayCounterTop);
+                if (Objects.equals(this.topColourBox, "Blue")) {
+                    g2.setColor(Color.blue);
+                } else {
+                    g2.setColor(Color.red);
+                }
             }
         }
 
         // Draw the bottom square if it exists
         if (this.bottomSquarePosition != null) {
-            drawSquare(g2, this.bottomSquarePositionStart, this.rayCounter);
+            drawSquare(g2, this.bottomSquarePositionStart, this.rayCounterBottom);
+            if (Objects.equals(this.botColourBox, "Blue")) {
+                g2.setColor(Color.blue);
+            } else {
+                g2.setColor(Color.red);
+            }
             if(this.endTile!=null) {
-                drawSquare(g2, this.bottomSquarePositionEnd, this.rayCounter);
+                drawSquare(g2, this.bottomSquarePositionEnd, this.rayCounterBottom);
+                if (Objects.equals(this.botColourBox, "Blue")) {
+                    g2.setColor(Color.blue);
+                } else {
+                    g2.setColor(Color.red);
+                }
             }
         }
     }
 
     private void drawSquare(Graphics2D g2, Point position, int label) {
-       // if(this.endTile != null) {
-        //    g2.setColor(Color.RED);
-       // }
-       // else {
-            g2.setColor(Color.BLUE);
-        //}
-        g2.drawRect(position.x, position.y, 10, 10);
         g2.fillRect(position.x, position.y, 10, 10);
         g2.setColor(Color.WHITE);
+        g2.drawRect(position.x, position.y, 10, 10);
         g2.setFont(new Font("Arial", Font.BOLD, 10));
         g2.drawString(String.valueOf(label), position.x + 2, position.y + 8);
     }
