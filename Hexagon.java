18e869f7a523a8d495efa18fc72078ae8bb67b49
MattyPc <mateusz.jakobik@ucdconnect.ie>
2024-05-05 21:19:07 +0100

    hexagon and tile have basic javadocs now

--- Hexagon.java
+++ Hexagon.java
@@ -1,16 +1,28 @@
 import java.awt.*;
-
+/**
+ * Represents a hexagonal tile on the game board.
+ */
 public class Hexagon extends Tile {
     private static int[] deflectionTable = {0, 1, 0, 2, 5, 3, 4, 3};
     private static int circleRadius = 60;
     private static int topLeftToCenter = (int) Math.sqrt((double) (Board.HEXSIZE * Board.HEXSIZE));
     private boolean active = false;
     private boolean[] passedRays = {false, false, false, false, false, false};
+    /**
+     * Constructs a hexagon tile with given coordinates.
+     *
+     * @param x The x-coordinate of the hexagon.
+     * @param y The y-coordinate of the hexagon.
+     * @param z The z-coordinate of the hexagon.
+     */
 
     public Hexagon(int x, int y, int z) {
         super(x, y, z);
     }
 
+    /**
+     * Handles the action when the hexagon tile is clicked.
+     */
     @Override
     public void clicked() {
         if (this.value == 0) {
@@ -25,14 +37,27 @@ public class Hexagon extends Tile {
     }
 
     //allow to change "active" of hexagon
+    /**
+     * Sets the activity status of the hexagon tile.
+     *
+     * @param x The activity status to set.
+     */
     void setActive(Boolean x) {
         active = x;
     }
-
+    /**
+     * Gets the activity status of the hexagon tile.
+     *
+     * @return The activity status of the hexagon tile.
+     */
     Boolean getActive() {
         return active;
     }
-
+    /**
+     * Handles the reception of a ray by the hexagon tile.
+     *
+     * @param r The ray to be received.
+     */
     public void receiveRay(Ray r) {
         // temporarily show the path of the ray
         // this.clicked();
@@ -64,12 +89,22 @@ public class Hexagon extends Tile {
             this.getAdjacent(endDirection).receiveRay(r);
         }
     }
-
+    /**
+     * Draws the hexagon
+     *
+     * @param g2          The graphics context to draw on.
+     * @param showCircles Flag indicating whether to show circles of influence.
+     */
     public void drawBottom(Graphics2D g2, boolean showCircles) {
         Hexmech.drawHex(this.p.x, this.p.y, g2);
         Hexmech.fillHex(this.p.x, this.p.y, this.value, g2);
     }
-
+    /**
+     * Draws the top layer of the hexagon. If showCircles is true, it will also draw circle of influence
+     *
+     * @param g2          The graphics context to draw on.
+     * @param showCircles Flag indicating whether to show circles of influence.
+     */
     public void drawTop(Graphics2D g2, boolean showCircles) {
         if (showCircles) {
             Point topLeft = Hexmech.hexToPixel(this.p.x, this.p.y);
@@ -99,12 +134,19 @@ public class Hexagon extends Tile {
             }
         }
     }
-
+    /**
+     * Calculates whether the hexagon tile has been correctly selected.
+     *
+     * @return 1 if correctly selected, otherwise -1.
+     */
     public int correctlySelected() {
         return (this.value == 0 && !this.active) || (this.value == 120 && this.active) ? 1 : -1;
     }
 
     // just so that Edge can inherit it from Tile
+    /**
+     * Method here just so Edge is able to inherit it from Tile
+     */
     void drawRayMarker(Graphics2D g2) {
 
     }
